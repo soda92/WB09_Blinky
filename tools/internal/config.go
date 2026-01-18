81315cdce17f7ce18825541202a8724fe5793dc4
Jackson Wong <wjxa20152015@gmail.com>
2026-01-18 17:33:19 +0800

    update to package structure

---
+++ tools/internal/config.go
@@ -0,0 +1,21 @@
+package internal
+
+const (
+	// Default SDK Path (can be overridden via flags if needed, but hardcoded for now)
+	SDKPath = "/home/soda/STM32Cube/Repository/STM32Cube_FW_WB0_V1.4.0"
+	
+	// Project Paths
+	CoreDir       = "Core"
+	LibraryDir    = "Library"
+	DriversDir    = "Drivers"
+	MiddlewaresDir = "Middlewares"
+	STM32BLEDir   = "STM32_BLE"
+	BuildDir      = "build"
+	
+	// Destination sub-directories for Library
+	LibSrcDir = "Library/Src"
+	LibIncDir = "Library/Inc"
+)
+
+// Default Template Path (BLE Beacon)
+const DefaultTemplatePath = "/home/soda/STM32Cube/Repository/STM32Cube_FW_WB0_V1.4.0/Projects/NUCLEO-WB09KE/Applications/BLE/BLE_Beacon"
---
+++ tools/internal/utils.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func FileExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func CopyFile(src, dst string) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	// Create destination directory if it doesn't exist
+	if err := os.MkdirAll(filepath.Dir(dst), 0755); err != nil {
+		return err
+	}
+
+	destFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, sourceFile)
+	return err
+}
+
+func CopyDir(src, dst string) error {
+	// Using 'cp -r' for simplicity and speed on Linux
+	cmd := exec.Command("cp", "-r", src, dst)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func AppendToFile(path, content string) error {
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.WriteString(content)
+	return err
+}
+
+func ReplaceInFile(path, oldString, newString string) error {
+	input, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	output := strings.Replace(string(input), oldString, newString, -1)
+
+	err = os.WriteFile(path, []byte(output), 0644)
+	return err
+}
